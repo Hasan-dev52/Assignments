b16dba2744feefc2d4317f802d42047e5ca2d64b
Hasan-dev52 <1272261480.h@mitwpu.edu.in>
2026-09-17 22:33:52 +0530

    New change

--- Assignment5.py
+++ Assignment5.py
@@ -1,7 +1,10 @@
 import re
 
 pan = input("Enter your PAN number: ")
-if re.match(r'^[A-Z]{5}[0-9]{4}[A-Z]{1}$', pan):
+
+pattern = r'^[A-Z]{5}[0-9]{4}[A-Z]{1}$'
+
+if re.match(pattern, pan):
     print("Valid PAN number.")
 else:
     print("Invalid PAN number.")
