476edf9c83f106332311aee3e5a11d34db71614f
Hasan-dev52 <1272261480.h@mitwpu.edu.in>
2026-09-17 11:03:20 +0530

    Completed Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+students = {}
+students[127] = ("Andrew", "CSE", 85)
+students[128] = ("John", "CE", 92)
+students[129] = ("Alice", "ME", 95)
+students[130] = ("Bob", "CSE", 80)
+print("Final students records:", students)
